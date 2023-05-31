c70d2a869f3ece45913c7361d2de8bf4cd1a0c7b
Zachary Sain <zachsain@gmail.com>
2023-05-31 15:00:21 -0500

    chatpage time date, css

--- client/src/ChatBox.js
+++ client/src/ChatBox.js
@@ -3,14 +3,20 @@ import { Avatar } from '@mui/material'
 import { Link } from 'react-router-dom'
 import "./ChatBox.css"
 
-function Chat({name, message, profilePic, timestamp, id}) {
+function Chat({name, message, profilePic, timestamp, id, userName}) {
+
   return (
     <Link to={`/chat/${id}`}>
     <div className="chat">
         <Avatar className="chat__image" alt={name} src={profilePic} />
         <div className="chat__details">
             <h2>{name}</h2>
-            <p>{message}</p>
+            {message ? (
+             <p>{message}</p>
+          ) : (
+            <p id="chat__noMessage" > Say Hello 👋  </p>
+            // <span className="handshake-emoji">🤝</span>
+          )}
         </div>
         <p className="chat__timestamp"> {timestamp} </p>
     </div>
--- client/src/ChatPage.js
+++ client/src/ChatPage.js
@@ -5,30 +5,49 @@ import ChatBox from './ChatBox';
 
 function ChatPage() {
   const { user, matches } = useContext(AppContext);
-  // console.log(matches);
+
+  function getTimeDifference(timestamp) {
+    const currentTime = new Date();
+    const messageTime = new Date(timestamp);
+    const difference = Math.floor((currentTime - messageTime) / 1000); 
+  
+    if (difference < 60) {
+      return 'Just now';
+    } else if (difference < 3600) {
+      const minutes = Math.floor(difference / 60);
+      return `${minutes} ${minutes === 1 ? 'min' : 'mins'} ago`;
+    } else if (difference < 86400) {
+      const hours = Math.floor(difference / 3600);
+      return `${hours} ${hours === 1 ? 'hour' : 'hours'} ago`;
+    } else {
+      const days = Math.floor(difference / 86400);
+      return `${days} ${days === 1 ? 'day' : 'days'} ago`;
+    }
+  }
 
   const box = matches
     ? matches.map((p) => {
-        let mostRecentMessage = null;
+        let mostRecentMessage = "";
         p.matches.forEach((match) => {
           const messages = p.messages.filter((m) => m.match_id === match.id);
-          console.log(messages)
           if (messages.length > 0) {
             const latestMessage = messages[messages.length - 1];
-            console.log(latestMessage)
-            if (!mostRecentMessage || new Date(latestMessage.created_at) > new Date(mostRecentMessage.created_at)) {
-              mostRecentMessage = latestMessage;
-            }
+            mostRecentMessage = latestMessage;
           }
         });
-        // const timestamp = new Date(mostRecentMessage.created_at).toLocaleTimeString();
-        console.log(mostRecentMessage);
-        // console.log(p);
+
+        const timestamp = mostRecentMessage ? getTimeDifference(mostRecentMessage.created_at) : null;
+        const matchDate = new Date(p.matches[0].created_at);
+        // const formattedTime = new Date(p.matches[0].created_at).toLocaleTimeString([], { hour: 'numeric', minute: '2-digit' });
+        const formattedTime = getTimeDifference(p.matches[0].created_at)
+        // const formattedTime = `${matchTime.getHours()}:${matchTime.getMinutes()}`;
+
         return (
           <ChatBox
+            userName={user.profile.first_name}
             name={p.profile.first_name}
-            // message={mostRecentMessage.content}
-            // timestamp={timestamp}
+            message={mostRecentMessage.content}
+            timestamp={mostRecentMessage ? timestamp : formattedTime}
             profilePic={p.profile.featured_image.url}
             id={p.id}
           />
@@ -40,5 +59,6 @@ function ChatPage() {
 }
 
 export default ChatPage;
+
 
 
